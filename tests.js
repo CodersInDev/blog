b70de8ae204ebde807b6abd73952fa2c04aca5cf
rub1e <alex.rubner@gmail.com>
2015-05-14 17:22:53 +0100

    updated tests again

--- tests.js
+++ tests.js
@@ -1,5 +1,19 @@
 // test comment!!
 
+test("Check the document has loaded fully", function(){
+//assign the iframe object to a varaiabl called iframe
+	var iframe = document.getElementById("iframe");
+//extract the contents(DOM) of the iframe and assign to var target
+	var target = iframe.contentDocument || iframe.contentWindow.document;
+//find the element with id hello in the iframe and get its contents
+	var ready = target.readyState;
+
+	equal(ready, "complete", "it works!")
+
+});
+
+
+
 test("Check the body has a background image", function(){
 //assign the iframe object to a varaiabl called iframe
 	var iframe = document.getElementById("iframe");
@@ -60,3 +74,24 @@ test("Check there is an element with ID featuredProj, and it has an image", func
 
 	equal(image, "Featured Project", "HELLYES")
 });
+
+
+
+setTimeout(function() { // this is only here so you can see the page change!
+  test("Check there is a link to meet the team and it goes to team.html", function(assert){
+    var done = assert.async();
+    var iframe = document.getElementById('iframe');
+    var target = iframe.contentDocument || iframe.contentWindow.document;
+    target.getElementById('team').children[0].click();
+
+    // wait for the next page to load
+    setTimeout(function() {
+      var iframe = document.getElementById('iframe');
+      var target = iframe.contentDocument || iframe.contentWindow.document;
+      var alex = target.getElementsByTagName('h3')[0].innerHTML;
+      // console.log('>>> '+hello)
+      equal(alex, 'Alex', 'as expected');
+      done();
+    }, 200);
+  });
+}, 2000); 
